610371f1030554f239ab77a767961ce467fbf240
Shalbin MS <shalbinms88@gmail.com>
2026-07-02 13:04:52 +0530

    feat: implement app routing and authentication architecture

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,10 +3,15 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
-import "./styles/index.css";
+import "./index.css";
+import { AuthProvider } from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
+  </React.StrictMode>,
 );
